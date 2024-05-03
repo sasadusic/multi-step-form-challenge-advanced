d7b73c4e44fbdc5b8f5a090e283c24d1fee8ec0d
sasadusic <sasadusic01101989@gmail.com>
2024-05-03 16:58:01 +0200

    m

--- main.js
+++ main.js
@@ -1,26 +1,51 @@
 const steps = document.querySelectorAll('.step')
 const prevs = document.querySelectorAll('.prev')
 const nexts = document.querySelectorAll('.next')
+const sideSteps = document.querySelectorAll('.side-step')
 
 let stepIndex = 1
+let sideStepIndex = 1
 
 nexts.forEach(next => {
     next.onclick = () => {
+        if(next.classList.contains('btn-step-3')){
+            document.querySelector('.choose-left').innerHTML = `${data.chooseName}(${data.type})`
+            document.querySelector('.choose-right').innerHTML = `$${data.choose}/${data.typName}`
+            document.querySelector('.total-type').innerHTML = data.type
+            document.querySelector('.total-num').innerHTML = `+$${data.choose}/${data.typName}`
+        }
         stepIndex++
+        sideStepIndex++
+        sideStepIndex = sideStepIndex > 4 ? 4 : sideStepIndex
         steps.forEach(step => {
             step.style.display = 'none'
         })
         steps[stepIndex - 1].style.display = 'block'
+
+        sideSteps.forEach(sideStep => {
+            if(sideStep.classList.contains('side-step-active')){
+                sideStep.classList.remove('side-step-active')
+            }
+        })
+        document.querySelector(`.side-step-${sideStepIndex}`).classList.add('side-step-active')
     }
 })
 
 prevs.forEach(prev => {
     prev.onclick = () => {
         stepIndex--
+        sideStepIndex--
         steps.forEach(step => {
             step.style.display = 'none'
         })
         steps[stepIndex - 1].style.display = 'block'
+
+        sideSteps.forEach(sideStep => {
+            if(sideStep.classList.contains('side-step-active')){
+                sideStep.classList.remove('side-step-active')
+            }
+        })
+        document.querySelector(`.side-step-${sideStepIndex}`).classList.add('side-step-active')
     }
 })
 
@@ -31,11 +56,79 @@ let data = {
     email: '',
     phone: '',
     type: 'monthly',
+    arcade: 9,
+    advance: 12,
+    pro: 15,
+    typName: 'mo',
+    service: 1,
+    storage: 2,
+    profile: 2,
+    choose: 0,
+    chooseName: 'arcade'
 }
+
+const chooseTypes = document.querySelectorAll('.choose-type')
+const arcade = document.querySelector('.arcade-num')
+const advance = document.querySelector('.advance-num')
+const pro = document.querySelector('.pro-num')
+
+const addType = document.querySelectorAll('.add-type')
+const service = document.querySelector('.service-num')
+const storage = document.querySelector('.storage-num')
+const provile = document.querySelector('.provile-num')
 
 
 toggle.onclick = () => {
     toggle.classList.toggle('yearly')
     data.type = data.type === 'monthly' ? 'yearly' : 'monthly'
-    alert(data.type)
+    if(data.type === 'yearly'){
+        data.typName = 'yr'
+        data.arcade = 90
+        data.advance = 120
+        data.pro = 150
+
+        data.service = 10
+        data.storage = 20
+        data.profile = 20
+    }else{
+        data.typName ='mo'
+        data.arcade = 9
+        data.advance = 12
+        data.pro = 15
+
+        data.service = 1
+        data.storage = 2
+        data.profile = 2
+    }
+    chooseTypes.forEach(chooseType => chooseType.innerText = data.typName)
+    arcade.innerText = data.arcade
+    advance.innerText = data.advance
+    pro.innerText = data.pro
+
+    addType.forEach(addType => addType.innerText = data.typName)
+    service.innerText = data.service
+    storage.innerText = data.storage
+    provile.innerText = data.profile
 }
+
+// Choose
+const cards = document.querySelectorAll('.choose-card')
+const arcadeCard = document.querySelector('.arcade')
+const advanceCard = document.querySelector('.advance')
+const proCard = document.querySelector('.pro')
+
+cards.forEach(card => {
+    card.onclick = () => {
+        cards.forEach(card => {
+            card.classList.remove('choose-card-active')
+        })
+        card.classList.add('choose-card-active')
+        const curChoose = card.dataset.choose
+        data.choose = data[curChoose]
+        data.chooseName = curChoose
+        // alert(data.choose)
+    }
+})
+
+// Summary
+const btnStep3 = document.querySelector('.btn-step-3')
